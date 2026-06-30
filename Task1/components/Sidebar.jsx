e5b5e38d844aa0f05bffdf2cfed10ef2611ecce5
Disha-kumari <dishakamin@gmail.com>
2026-06-30 21:15:22 +0530

    Create Sidebar.jsx

---
+++ Task1/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+ function Sidebar() {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>Dashboard</li>
+        <li>Issues</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+}
+export default Sidebar;
